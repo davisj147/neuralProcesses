7b7e2497a0d0202fde3985a3be2db752aa5969b3
yk0987 <78436398+yk0987@users.noreply.github.com>
2021-03-05 10:14:57 +0000

    Update pl_datasets.py

--- pl_datasets.py
+++ pl_datasets.py
@@ -3,14 +3,15 @@ import pytorch_lightning as pl
 from torch.utils.data.dataloader import default_collate
 from torch.utils.data import DataLoader
 
-from datasets import GPData, SineData, mnist
+from datasets import GPData, SineData, ImgDataset
 
 
 class NPBaseDataModule(pl.LightningDataModule):
     def __init__(self, dataset, num_workers=4, batch_size=4):
         self.dataset = dataset
         self.x_dim = dataset.x_dim
-        self.y_dim = dataset.x_dim
+        self.y_dim = dataset.y_dim
+        self.img_size = dataset.img_size
 
         self.num_workers = num_workers
         self.batch_size = batch_size
@@ -22,19 +23,6 @@ class NPBaseDataModule(pl.LightningDataModule):
         self.prepare_data()
         self.setup()
 
-    def __init__(self, train_loader, test_loader, img_size, num_workers=4, batch_size=4):
-        self.img_size = img_size # instead of x and y dim, use [1,28,28] for mnist / [3,32,32] for celeba
-
-        self.num_workers = num_workers
-        self.batch_size = batch_size
-
-        self.train_loader = train_loader
-        self.val_loader = None
-        self.test_loader = test_loader
-
-        self.prepare_data()
-
-
     @property
     def collate_fn(self):
         return default_collate
@@ -45,6 +33,7 @@ class NPBaseDataModule(pl.LightningDataModule):
     def setup(self):
         # TODO split into train and validation set. At the moment, validation data is
         #  the same as training data
+
         self.train_loader = DataLoader(self.dataset, batch_size=self.batch_size,
                                        num_workers=self.num_workers,
                                        shuffle=True)
@@ -69,29 +58,22 @@ class NPBaseDataModule(pl.LightningDataModule):
 
 class NPDataModule(NPBaseDataModule):
     def __init__(self, dataset_type, num_workers=4, batch_size=4, **kwargs):
-        assert dataset_type in ['sine', 'gpdata','mnist']
+        assert dataset_type in ['sine', 'gpdata', 'mnist', 'celeb']
         self.dataset_type = dataset_type
-        if dataset_type == 'sine' or 'gpdata':
-            dataset = self._get_dataset(dataset_type, **kwargs)
-            super().__init__(dataset=dataset, num_workers=num_workers, batch_size=batch_size)
-        else:
-            train_loader, test_loader = self._get_dataloader(dataset_type, **kwargs)
-            super().__init__(train_loader=train_loader, test_loader=test_loader, num_workers=num_workers, batch_size=batch_size)
+        dataset = self._get_dataset(dataset_type, **kwargs)
 
-
+        super().__init__(dataset=dataset, num_workers=num_workers, batch_size=batch_size)
 
     def _get_dataset(self, dataset_type, **kwargs):
+
         if dataset_type == 'sine':
             dataset = SineData(**kwargs)
         elif dataset_type == 'gpdata':
             dataset = GPData(**kwargs)
+        elif dataset_type == 'mnist':
+            dataset = ImgDataset('mnist', batch_size, **kwargs)
+        elif dataset_type == 'celeb':
+            dataset = ImgDataset('celeb', batch_size, **kwargs)
+
         return dataset
-    
-    def _get_dataloader(self, dataset_type, **kwargs):
-        if dataset_type == 'mnist':
-            train_loader, test_loader = mnist()
-            img_size = [1,28,28]
-        elif dataset_type == 'celeba'
-            ## to be installed
-        
-        return train_loader, test_loader, img_size
+
