7867813a4755e45a0153d568a349aa42ad6bed84
G. Petrulionyte <gintepe@users.noreply.github.com>
2021-03-17 11:33:29 +0000

    change std floor

--- np.py
+++ np.py
@@ -128,7 +128,8 @@ class LinearDecoder(nn.Module):
 
         # this as per Emp. evaluation of NP objectives
         # can't seem to find out what the NP paper did exactly
-        sigmas = 0.1 + 0.9 * nn.functional.softplus(sigmas)
+        # sigmas = 0.1 + 0.9 * nn.functional.softplus(sigmas)
+        sigmas = 0.01 + 0.99 * nn.functional.softplus(sigmas)
 
         return means, sigmas
 
@@ -191,7 +192,8 @@ class LinearRToDist(nn.Module):
 
         # this as per Emp. evaluation of NP objectives and the tf notebook
         # can't seem to find out what the NP paper did exactly
-        sigmas = 0.1 + 0.9 * torch.sigmoid(sigmas)
+        # sigmas = 0.1 + 0.9 * torch.sigmoid(sigmas)
+        sigmas = 0.01 + 0.99 * torch.sigmoid(sigmas)
 
         return means, sigmas
 
