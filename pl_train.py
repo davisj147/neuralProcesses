89e8f1f2543c52182bc13ab5f15b687527534ed9
yk0987 <78436398+yk0987@users.noreply.github.com>
2021-03-18 14:16:45 +0000

    Update pl_train.py

--- pl_train.py
+++ pl_train.py
@@ -7,9 +7,6 @@ from pl_callbacks import WandbLogPriorPosteriorSamplePlots
 from typing import List
 import torch
 
-import wandb
-wandb.init(project="mlmi4-np", entity="yk87")
-
 import argparse
 
 parser = argparse.ArgumentParser(description='Train video action recognition models.')
@@ -17,54 +14,65 @@ parser.add_argument('--dataset_type', type=str, default='sine',
                     choices=['sine', 'gpdata', 'mnist', 'celeb'], help='Dataset name')
 parser.add_argument('--num_workers', type=int, default=0,
                     help='Number of CPU cores to load data on')
-parser.add_argument('--max_epochs', type=int, default=100,
+parser.add_argument('--batch_size', type=int, default=8,
+                    help='Data batch size')
+parser.add_argument('--num_context', type=int, default=8,
+                    help='Number of Context datapoints')
+parser.add_argument('--num_target', type=int, default=16,
+                    help='Number of Target datapoints (Context + Target by convention)')
+parser.add_argument('--r_dim', type=int, default=50,
+                    help='Dimension of encoder representation of context points')
+parser.add_argument('--z_dim', type=int, default=50,
+                    help='Dimension of sampled latent variable')
+parser.add_argument('--h_dim', type=int, default=50,
+                    help='Dimension of hidden layer in encoding to gaussian mean/variance network')
+parser.add_argument('--h_dim_enc', type=int, nargs='*', default=[50, 50],
+                    help='Dimension(s) of hidden layer(s) in encoder')
+parser.add_argument('--h_dim_dec', type=int, nargs='*', default=[50, 50, 50],
+                    help='Dimension(s) of hidden layer(s) in decoder')
+parser.add_argument('--max_epochs', type=int, default=20,
                     help='Maximum number of training epochs')
 parser.add_argument('--cpu', action='store_true',
                     help='Whether to train on the CPU. If ommited, will train on a GPU')
+parser.add_argument('--lr', type=float, default=1e-3,
+                    help='Initial learning rate')
 parser.add_argument('--tune-lr', action='store_true',
                     help='Whether to automatically tune the learning rate prior to training')
-parser.add_argument('--batch_size', type=int, default=16,
-                    help='Data batch size')
-parser.add_argument('--num_context', type=int, default=200,
-                    help='Number of Context datapoints')
-parser.add_argument('--num_target', type=int,default=200,
-                    help='Number of Target datapoints (Context + Target by convention)')
-parser.add_argument('--r_dim', type=int, default=256,
-                    help='Dimension of encoder representation of context points')
-parser.add_argument('--z_dim', type=int, default=256,
-                    help='Dimension of sampled latent variable')
-parser.add_argument('--h_dim', type=int, default=256,
-                    help='Dimension of hidden layer in encoding to gaussian mean/variance network')
-parser.add_argument('--h_dim_enc', type=int, nargs='*', default=[256*4, 256*4],
-                    help='Dimension(s) of hidden layer(s) in encoder')
-parser.add_argument('--h_dim_dec', type=int, nargs='*', default=[256*4, 256*4],
-                    help='Dimension(s) of hidden layer(s) in decoder')
-parser.add_argument('--lr', type=float, default=5e-05,
-                    help='Initial learning rate')
-
-    
-parser.add_argument('--img_size', type=int, nargs='*', default=[1,28,28],  ###
-                    help='Image size (mnist=[1,28,28],celeb=[3,32,32])')
-
+parser.add_argument('--gp-kernel', type=str, default='rbf',
+                    choices=['rbf', 'matern', 'periodic'],
+                    help='kernel to use for the gaussian process dataset generation')
+parser.add_argument('--n-samples', type=int, default=2000,
+                    help='number of samples for sine or gp dataset generation')
+parser.add_argument('--n-points', type=int, default=100,
+                    help='number of points per sample samples for sine or gp dataset generation')
+parser.add_argument('--lengthscale-range', type=float, nargs='*', default=[0.25, 0.5],
+                    help='Range for lengthscales when generating GP data')
+parser.add_argument('--sigma-range', type=float, nargs='*', default=[1., 1.],
+                    help='Range for sigma (output variance) when generating GP data')
+parser.add_argument('--period-range', type=float, nargs='*', default=[1., 1.],
+                    help='Range for periods when generating GP data')
 
 args = parser.parse_args()
 
 if __name__ == '__main__':
-    print(args.num_context)
-        
     # ------------------------
     # 1 SETUP DATA MODULES
     # ------------------------
     dm = NPDataModule(dataset_type=args.dataset_type,
                       num_workers=args.num_workers,
-                      batch_size=args.batch_size)
+                      batch_size=args.batch_size,
+                      kernel=args.gp_kernel,
+                      num_samples=args.n_samples,
+                      num_points=args.n_points,
+                      lengthscale_range=args.lengthscale_range,
+                      sigma_range=args.sigma_range,
+                      period_range=args.period_range)
 
     # ------------------------
     # 2 INIT LIGHTNING MODEL
     # ------------------------
     model = PLNeuralProcess(x_dim=dm.x_dim,
                             y_dim=dm.y_dim,
-                            img_size=dm.img_size,   ##
                             lr=args.lr,
                             num_context=args.num_context,
                             num_target=args.num_target,
@@ -84,10 +92,9 @@ if __name__ == '__main__':
                          checkpoint_callback=ModelCheckpoint(
                              monitor='validation_loss',
                              filename='neural_process-{epoch:02d}-{validation_loss:.2f}'),
-                         #logger=WandbLogger(project=f'AdvancedML-{args.dataset_type}',
-                         #                   log_model=True),
-                         logger=WandbLogger(project=f'mlmi4-np', log_model=True),
-
+                         logger=WandbLogger(project=f'AdvancedML-{args.dataset_type}',
+                                            log_model=True),
+                         auto_lr_find=args.tune_lr,
                          callbacks=cbs)
 
     # ------------------------
